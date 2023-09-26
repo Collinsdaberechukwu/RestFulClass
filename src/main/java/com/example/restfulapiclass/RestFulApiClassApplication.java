03803dec6ec04d80f77cbcbc9fdcfcd61250e266
Collins Daberechukwu <collinsdaberechi20@gmail.com>
2023-09-26 04:27:45 +0100

    first update

---
+++ src/main/java/com/example/restfulapiclass/RestFulApiClassApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfulapiclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFulApiClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestFulApiClassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restfulapiclass/RestFulApiClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfulapiclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFulApiClassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
