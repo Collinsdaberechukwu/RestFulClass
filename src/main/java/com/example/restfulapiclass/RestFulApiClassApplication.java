c84e4aeab68ac68c92fc29407b4bb64ea12a7852
Collins Daberechukwu <collinsdaberechi20@gmail.com>
2023-09-26 04:31:59 +0100

    change

--- src/main/java/com/example/restfulapiclass/RestFulApiClassApplication.java
+++ src/main/java/com/example/restfulapiclass/RestFulApiClassApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestFulApiClassApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(RestFulApiClassApplication.class, args);
     }
